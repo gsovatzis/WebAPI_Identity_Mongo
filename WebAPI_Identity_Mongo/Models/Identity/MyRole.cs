04acfb346a1ef42f06c8ddc0ed186b5879c54ade
George Sovatzis <gsovatzis@hotmail.com>
2019-12-10 16:22:11 +0200

    Add project files.

---
+++ WebAPI_Identity_Mongo/Models/Identity/MyRole.cs
@@ -0,0 +1,12 @@
+﻿using AspNetCore.Identity.Mongo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Identity_Mongo.Models.Identity
+{
+    public class MyRole : MongoRole
+    {
+    }
+}
---
+++ WebAPI_Identity_Mongo/Models/Identity/MyUser.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.Identity.Mongo.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Identity_Mongo.Models.Identity
+{
+    public class MyUser : MongoUser
+    {
+        [PersonalData]
+        public string FirstName { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
+    }
+}
